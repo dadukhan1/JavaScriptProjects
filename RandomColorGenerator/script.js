0ec33da1cd566cc40082934d92032d3966930572
Dadu Khan <photoshopt65@gmail.com>
2024-12-10 20:19:36 -0800

    just Copy Button left

--- RandomColorGenerator/script.js
+++ RandomColorGenerator/script.js
@@ -14,8 +14,22 @@ hexButton.addEventListener("click", () => {
     );
   }
 
-  hexColorValue.textContent = `${hexColorCode}`;
+  hexColorValue.textContent = `#${hexColorCode}`;
   hexColorGeneratorBackground.style.backgroundColor = `#${hexColorCode}`;
 
-  console.log("The code is : ", hexColorCode);
+});
+
+const rgbButton = document.querySelector(".rgb-button");
+const getRedInputRange = document.querySelector("#red");
+const getGreenInputRange = document.querySelector("#green");
+const getBlueInputRange = document.querySelector("#blue");
+
+const rgbBackground = document.querySelector(".rgb-color-generator");
+
+rgbButton.addEventListener("click", () => {
+  let exactRedValue = getRedInputRange.value;
+  let exactGreenValue = getGreenInputRange.value;
+  let exactBlueValue = getBlueInputRange.value;
+
+  rgbBackground.style.backgroundColor = `rgb(${exactRedValue},${exactGreenValue},${exactBlueValue})`;
 });
